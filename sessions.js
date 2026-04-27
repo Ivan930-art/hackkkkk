18283ac65bd3650e7564b5637c64550372b15337
IvanC <ivanchen2492@gmail.com>
2026-04-27 11:19:14 -0400

    Delete sessions.js

--- sessions.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-    const sessions = [
-        "Math Study Group - 4PM",
-        "JavaScript Practice - 6PM",
-        "Physics Help Room - 7PM"
-    ];
-
-    res.status(200).json(sessions);
-}
